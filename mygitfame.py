611cba0c1fe8211680691835d111ca4cc87b9ab8
seongho9.lee <seongho9.lee@lge.com>
2022-01-18 17:35:39 +0900

    [wip] fix #3 branch creation date manually..

--- mygitfame.py
+++ mygitfame.py
@@ -12,11 +12,11 @@ check_branch = ['--branch=origin/event/4.0.12xx', '--branch=origin/event/4.0.11x
                 '--branch=origin/event/3.6.11xx', '--branch=origin/event/3.6.10xx', '--branch=origin/event/3.5.18xx',
                 '--branch=origin/event/3.5.17xx', '--branch=origin/event/3.5.19xx',
                 '--branch=origin/event/3.5.15xx']
-since_date = ['--since=2021-12-22', '--since=2021-12-02', '--since=2021-12-14', '--since=2021-10-20',
-              '--since=2021-09-24',
-              '--since=2021-08-04', '--since=2021-08-03', '--since=2021-06-22', '--since=2021-05-04',
-              '--since=2021-04-30',
-              '--since=2021-02-26']
+since_date = ['--since=2021-12-13', '--since=2021-11-16', '--since=2021-11-05', '--since=2021-09-29',
+              '--since=2021-08-04',
+              '--since=2021-06-30', '--since=2021-06-09', '--since=2021-04-08', '--since=2021-03-12',
+              '--since=2021-04-07',
+              '--since=2021-02-02']
 out_format = '--format=json'
 # ===test data set====
 t_git_dir = 'c:/sho_ws/jira_creator'
@@ -27,4 +27,3 @@ t_since = ['--since=2022-01-17', '--since=2022-01-17']
 
 for i in range(len(check_branch)):
     gitfame.main(['--sort=commits', '-wt', check_branch[i], since_date[i], '-e', out_format, git_dir])
-
